8724f00eb36a835ed824fd34eb30a7fc7e26d1c7
cinob <cinob@foxmail.com>
2021-09-02 09:37:17 +0800

    🤔

---
+++ 10.3 lifetime/src/main.rs
@@ -0,0 +1,31 @@
+struct ImportantExcerpt<'a> {
+    part: &'s str
+}
+
+fn main() {
+    let string1 = String::from("abcd");
+    // let string2 = "xyz";
+
+    // let res = longest(string1.as_str(), string2);
+
+    let res;
+    {
+        let string2 = "xyz";
+        res = longest(string1.as_str(), string2);
+    }
+
+    println!("longest: {}", res);
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+// 使用参数x的生命周期
+// fn longest<'a>(x: &'a str, y: &str) -> &'a str {
+//     x
+// }
