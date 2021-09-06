a44ddff46ca6e6c8a83edabcce8d752279870738
cinob <cinob@foxmail.com>
2021-09-06 17:14:54 +0800

    🤔 重构代码结构

--- 12 minigrep/src/lib.rs
+++ 12 minigrep/src/lib.rs
@@ -1,20 +1,41 @@
 use std::fs;
 use std::error::Error;
 
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+pick three.";
+
+    assert_eq!(
+      vec!["safe, fast, productive."],
+      search(query, contents)
+    )
+  }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 pub struct Config {
     pub query: String,
     pub filename: String
 }
-
-// fn parse_config(args: &[String]) -> Config {
-//     let query = args[1].clone();
-//     let filename = args[2].clone();
-
-//     Config {
-//         query,
-//         filename
-//     }
-// }
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
@@ -31,7 +52,9 @@ impl Config {
 pub fn run(config: Config) ->Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
