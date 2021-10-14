27657eb80fe36454303cec423dc4e459875f5089
cinob <cinob@foxmail.com>
2021-10-14 10:49:51 +0800

    🎨 multi

--- hello/src/bin/main.rs
+++ hello/src/bin/main.rs
@@ -1,13 +1,20 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
 use std::fs;
+use std::thread;
+use std::time::Duration;
+use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -17,8 +24,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(3));
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT Found", "404.html")
